9291ab12c31b363935d0c904ec8ad5a91916a53d
Henrique Paes de Souza <hpaesdesouza@gmail.com>
2022-11-18 18:24:53 -0300

    business logic implemented!

--- src/app/components/booking-details/booking-details.component.ts
+++ src/app/components/booking-details/booking-details.component.ts
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking-details.component.css']
 })
 export class BookingDetailsComponent implements OnInit {
+  bookingDuration: number; // booking duration, coming from Reservation Object
+  carRent: number; // value of the rent, coming from the Car Object
 
-  constructor() { }
+  carProtection ?= false;
+  carProtectionValue: number;
+
+  fullProtection ?= false;
+  fullProtectionValue: number;
+
+  tpProtection ?= false;
+  tpProtectionValue: number;
+
+  glassProtection ?= false;
+  glassProtectionValue: number;
+
+  extras?: boolean;
+  extrasValue: number;
+
+  totalValue: number;
+
+  constructor() {
+    this.bookingDuration = 1;
+    this.carRent = 150;
+    this.totalValue = this.carRent * this.bookingDuration;
+   }
 
   ngOnInit(): void {
+  }
+
+  updateStatus() {
+    
+    const teste = document.getElementsByTagName("input")
+
+    this.carProtection = teste.namedItem("car-protection")?.checked;
+    this.fullProtection = teste.namedItem("full-protection")?.checked;
+    this.tpProtection = teste.namedItem("tp-protection")?.checked;
+    this.glassProtection = teste.namedItem("glass-protection")?.checked;
+
+    this.carProtectionValue = this.carProtection ? 10 : 0;
+    this.fullProtectionValue = this.fullProtection ? 20 : 0;
+    this.tpProtectionValue = this.tpProtection ? 5 : 0;
+    this.glassProtectionValue = this.glassProtection ? 2 : 0;
+
+    this.extras = this.carProtection || this.fullProtection || this.tpProtection || this.glassProtection;
+    this.extrasValue = this.bookingDuration * (this.carProtectionValue + this.fullProtectionValue + this.tpProtectionValue + this.glassProtectionValue);
+
+    this.totalValue = this.extras ? this.bookingDuration * this.carRent + this.extrasValue : this.bookingDuration * this.carRent;
+    
   }
 
 }
