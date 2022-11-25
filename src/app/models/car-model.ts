d086d898641bc870ecd59a3f69bb507a5456d120
Henrique Paes de Souza <hpaesdesouza@gmail.com>
2022-11-25 15:06:00 -0300

    dynamic listing

--- src/app/models/car-model.ts
+++ src/app/models/car-model.ts
@@ -11,5 +11,6 @@ export class Car{
     seats: number;
     trunk: number;
     consumption: number;
+    image: string;
 
 }
---
+++ src/app/services/car-register.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Car } from '../models/car-model';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarRegisterService {
+
+  constructor(private http: HttpClient) {}
+
+  registerCar(car: Car){
+    this.http.post<Car>("http://localhost:8080/car-creation/add-car", car).subscribe(
+      (response) => {
+        alert("succesfully registered car");
+        console.log(response);
+      }
+    );
+  }
+}
